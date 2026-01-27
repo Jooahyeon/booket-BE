a1f982f99dcee626aeb5f0cd235b9dc22125551f
Jooahyeon <dkgus0496@naver.com>
2026-01-27 22:43:01 +0900

    Initial Commit: Booket 초기 세팅

---
+++ src/main/java/com/hyggemind/booket/BooketApplication.java
@@ -0,0 +1,13 @@
+package com.hyggemind.booket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyggemind/booket/BooketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyggemind.booket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
