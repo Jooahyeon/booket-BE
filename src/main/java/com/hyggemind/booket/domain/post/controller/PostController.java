5a9d5d826a4656ed58ff17f695c6eb95e1a2549a
Jooahyeon <dkgus0496@naver.com>
2026-03-11 21:37:44 +0900

    feat: 엔터티수정

---
+++ src/main/java/com/hyggemind/booket/domain/post/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.hyggemind.booket.domain.post.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+//    @GetMapping("/post/{id}")
+//    public ResponseEntity<>
+}
